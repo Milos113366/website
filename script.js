e13f19003cd7b3718b2006556a72eb303fd68fa8
Milos113366 <123424577+Milos113366@users.noreply.github.com>
2023-01-30 11:10:25 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-window.alert('Ovo je sajt vremenske prognoze!!');
+//window.alert('Ovo je sajt vremenske prognoze!!');
+
+function tempcalculator(){
+
+    let unetoTemp = document.querySelector('#tempF');
+
+    let brojnoTemp = parseFloat(unetoTemp.value);
+
+    let tempC = (brojnoTemp - 32) * 5/9;
+    tempC = tempC.toFixed(1);
+
+        
+let newElement = document.createElement('div');
+newElement.className = 'new-value';
+newElement.innerText = `${brojnoTemp} stepeni Fahrenheit-a je isto sto i ${tempC} stepeni Celsius-a`;
+console.log(newElement.innerText);
+document.querySelector('.container').appendChild(newElement);
+
+}
